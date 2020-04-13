a01baa0e4cff9d7e013428a3cbf766e2e4bc50de
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-13 02:33:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 function forLoop(array){
   for(let i = 0; i < 25; i++){
-    return `I am ${i} strange loops.`
+    if(i == 1){
+      console.log("I am 1 strange loop")
+    }
+    else{
+    console.log( `I am ${i} strange loops.`)
+    }
   }
 }
 
